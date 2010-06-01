e15a327d8b00469344664334d6451f46a81f3ddc
Seth Ridley <Seth Ridley@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-06-01 14:36:18 +0000

    git-svn-id: https://plugins.svn.wordpress.org/kino-event-calendar-plugin/trunk@247802 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/main.js.php
+++ js/main.js.php
@@ -1,5 +1,34 @@
 <?php
-include $_SERVER['DOCUMENT_ROOT']."/wp-load.php";
+header("Content-type: text/javascript"); 
+/*************************************************************************************************/
+/*
+ * To any coders/maintainers:
+ * 
+ * Put this code at the top of each PHP file that needs to know paths to other resources.
+ * Modify the path to ke-location.php to be relative to the plugin tree structure. For example:
+ *		"/../ke-location.php"
+ *		"/../../ke-location.php"
+ *		etc.
+ * 
+ * See <plugin_directory>/ke-location.php comments for docs of the expected return values for 
+ * the variables.
+ *
+ */
+include_once dirname(__FILE__)."/../ke-location.php";
+
+// It's a good idea that if ke-location.php is extended, make explicit global variables 
+// for these. Don't rely on variables automatically being global as they won't be when you
+// least expect it, leading to INCREDIBLY difficult-to-find errors.
+global $pluginLocation, $pluginRelativeLocation, $pluginDirname, $wpBaseLocation, $theBaseURL, $myURL;
+
+$pluginLocation = ke_getPluginLocation();
+$pluginRelativeLocation = ke_getPluginRelativeLocation();
+$wpBaseLocation = ke_getInstallBaseLocation();
+$pluginDirname = ke_getPluginDirname();
+$theBaseURL = ke_getWPURL();
+$myURL = ke_getMyURLDir();
+/*************************************************************************************************/
+include $wpBaseLocation."/wp-load.php";
 ?>
 // JavaScript Document
 var j = jQuery.noConflict();
@@ -20,7 +49,8 @@ j(document).ready(function()
 		events: function(start, end, callback) {
 		
 			// do some asynchronous ajax
-			j.getJSON("/wp-content/plugins/kino-event-calendar-plugin/ke-rpc.php",
+			// bmb
+			j.getJSON("<?php global $pluginRelativeLocation; echo $pluginRelativeLocation; ?>/ke-rpc.php",
 			{
 				action: "ke_get_events",
 				start: start.getTime(),
